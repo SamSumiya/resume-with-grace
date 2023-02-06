9b1f22eff47aa86945dd6ff2a65543df20b151aa
Adrian Apodaca <aapodaca@shroodbi.com>
2023-02-05 20:36:23 -0800

    feat(add sample server in express.): add sample server in express

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
+
+
+
+
 import store from './store/store'
+// require ('dotenv').config()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      {console.log(store.getState())}
+      {/* {console.log(store.getState())}
+      {console.log(import.meta.env)} */}
       <App />
     </Provider>
   </React.StrictMode>,
